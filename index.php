c3aa34e3b10aea24d1b456534ea061862e89bb51
chaitrapatwardhan <68545255+chaitrapatwardhan@users.noreply.github.com>
2021-03-05 15:40:11 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,50 @@
+<!doctype html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- Bootstrap CSS -->
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
+    <!--ICON-->
+        <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+    <!--FontAwesome-->
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <!--CSS Pages-->    
+        <link rel="stylesheet" type="text/css" href="css/navbar.css">
+        <link rel="stylesheet" type="text/css" href="css/index.css">
+
+        <title>Grips Bank</title>
+    </head>
+    <body>
+
+        <!--Includes the contents from the navbar.php page-->
+        <?php
+          include 'navbar.php';
+        ?>
+
+    <div class="text-center"><br>
+    <h1 class="text">Welcome To Grips Bank</h1>
+    </div>
+
+    <div class="imgbank">
+        <img src="https://media.istockphoto.com/vectors/bank-modern-thin-line-design-style-vector-illustration-vector-id1031507604?b=1&k=6&m=1031507604&s=612x612&w=0&h=7mqx08ckUAoIQGaDJsiK7KaEmRrRhZuYkuUQ262P1XU=" alt="Bank">
+    </div>
+
+    <footer class="footer">
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-auto">
+                <p class="ant"><b>Chaitra Patwardhan</b></p>
+                    <a class="btn btn-social-icon btn-linkedin ant"  href="https://www.linkedin.com/in/chaitra-patwardhan-15a23b1aa/"><i class="fa fa-linkedin "></i></a>                      
+                    <a class="btn btn-social-icon btn-instagram"  href="https://www.instagram.com/ChaitraPatwardhan/"><i class="fa fa-instagram"></i></a>
+                    <a class="btn btn-social-icon btn-github" href="https://github.com/chaitrapatwardhan"><i class="fa fa-github"></i></a> 
+                <p class="ant">#GRIPMARCH21</p>                   
+            </div>
+        </div>
+    </div>
+    </footer>
+    <!-- Bootstrap JavaScript -->
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.6.0/dist/umd/popper.min.js" integrity="sha384-KsvD1yqQ1/1+IA7gi3P0tyJcT3vR+NdBTt13hSJ2lnve8agRGXTTyNaBYmCR/Nwi" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.min.js" integrity="sha384-nsg8ua9HAw1y0W1btsyWgBklPnCUAFLuTMS2G72MMONqmOymq585AcH49TLBQObG" crossorigin="anonymous"></script>
+    </body>
+</html>
